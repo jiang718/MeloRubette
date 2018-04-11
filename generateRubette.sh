637afc09f8f05106fc3095be84f684d95674c366
Mijia Jiang <jiang718@umn.edu>
2018-04-10 22:27:14 -0500

    change folder structure

--- generateRubette.sh
+++ generateRubette.sh
@@ -1,6 +1,6 @@
-rm -f MeloSubMotifRubette.class MeloSubMotifRubette.jar
+rm -f MeloSubMotifRubette.class Note.class melo.jar
 rm /home/nil/.rubato/plugins/melo.jar
-javac -classpath ../rubato.jar melo/MeloSubMotifRubette.java melo/Note.java
-jar cvmf MeloManifest melo.jar melo
+javac -classpath ../rubato.jar MeloSubMotifRubette.java Note.java
+jar cvmf MeloManifest melo.jar . 
 cp melo.jar /home/nil/.rubato/plugins/
 java -Djava.library.path=../ -jar ../rubato.jar
