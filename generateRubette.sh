df2282d44a64727ad4f58a0e5de426bbadb9a364
Mijia Jiang <jiang718@umn.edu>
2018-04-10 22:31:57 -0500

    change structrue

--- generateRubette.sh
+++ generateRubette.sh
@@ -1,6 +1,6 @@
-rm -f MeloSubMotifRubette.class Note.class melo.jar
+rm -f melo/MeloSubMotifRubette.class melo/Note.class melo.jar
 rm /home/nil/.rubato/plugins/melo.jar
-javac -classpath ../rubato.jar MeloSubMotifRubette.java Note.java
-jar cvmf MeloManifest melo.jar . 
+javac -classpath ../rubato.jar melo/MeloSubMotifRubette.java melo/Note.java
+jar cvmf MeloManifest melo.jar melo 
 cp melo.jar /home/nil/.rubato/plugins/
 java -Djava.library.path=../ -jar ../rubato.jar
